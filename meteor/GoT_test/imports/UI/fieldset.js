4f80059541559c28ba1426b5d47b7959a0fa3b59
Jean Ceppi <jeanceppi@protonmail.com>
2017-03-22 14:27:31 +0100

    Passage de paramètres dans fieldset

--- meteor/GoT_test/imports/UI/fieldset.js
+++ meteor/GoT_test/imports/UI/fieldset.js
@@ -2,6 +2,15 @@ import { Template } from 'meteor/templating';
 
 import './fieldset.html';
 
+// Le helper permet d'exécuter du code JS dans le template
+Template.fieldset.helpers({
+   
+   // Création du tag "fs_title"
+   'fs_title' : function(title){
+       return title;
+    }
+});
+
 Template.fieldset.events({
   'click .fieldset'() {
       alert('test');
