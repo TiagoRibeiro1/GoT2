2c359565c43c5d659c665377c7b277d93bbb032f
Jean Ceppi <jeanceppi@protonmail.com>
2017-03-22 15:14:00 +0100

    package iron:router

--- meteor/GoT_test/imports/UI/fieldset.js
+++ meteor/GoT_test/imports/UI/fieldset.js
@@ -4,15 +4,16 @@ import './fieldset.html';
 
 // Le helper permet d'exécuter du code JS dans le template
 Template.fieldset.helpers({
-   
-   // Création du tag "fs_title"
-   'fs_title' : function(title){
-       return title;
-    }
+ 
 });
 
 Template.fieldset.events({
-  'click .fieldset'() {
-      alert('test');
-  },
+    
+    'submit .btn_submit'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+    },
+    'submit .creer'(event) {
+        
+    },
 });
