6353744b6a0959593dbda76a0d3de23ba133c669
Raphbub <raph.bub@gmail.com>
2017-03-23 15:42:21 +0100

    Tests routing avec iron:routing

--- meteor/GoT_test/client/route.js
+++ meteor/GoT_test/client/route.js
@@ -1,8 +1,16 @@
+Router.configure({
+  layoutTemplate: 'navBar',
+  loadingTemplate: 'navBar'
+});
+
 Router.route('/', function () {
   this.render('homepage');
 });
- 
+
 Router.route('/creer', function () {
   this.render('page_creer');
 });
- 
+
+Router.route('/suivre', function () {
+  this.render('page_suivre');
+});
--- meteor/GoT_test/imports/UI/fieldset.js
+++ meteor/GoT_test/imports/UI/fieldset.js
@@ -4,17 +4,17 @@ import './fieldset.html';
 
 // Le helper permet d'exécuter du code JS dans le template
 Template.fieldset.helpers({
- 
+
 });
 
 Template.fieldset.events({
-    
+
     'submit .btn_submit'(event) {
         // Prevent default browser form submit
         event.preventDefault();
     },
-    'submit .creer'(event) {
-        // À MODIFIER (cf. route.js & layoutApp.html)
-        window.open('/creer','_self');
-    },
+    // 'submit .creer'(event) {
+    //     // À MODIFIER (cf. route.js & layoutApp.html)
+    //     window.open('/creer','_self');
+    // },
 });
