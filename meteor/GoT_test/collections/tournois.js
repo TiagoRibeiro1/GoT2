f19a35190a7c600230789f46ca1c163e8b8ec698
lucas.martinez787@gmail.com <lucas.martinez787@gmail.com>
2017-04-04 14:55:16 +0200

    finished autoform and conditions

--- meteor/GoT_test/collections/tournois.js
+++ meteor/GoT_test/collections/tournois.js
@@ -25,7 +25,15 @@ Tournois.attachSchema(new SimpleSchema({
   optionChmpt: {
     type: Number,
     label: "Nombre de match contre chaque équipe",
-    max: 8
+    max: 8,
+    min: 2,
+    optional: true,
+    custom: function(){
+      // If tournament is Championnat or championat & playoff
+      if(this.field("typeTournoi").value !== "ELD" && this.value === undefined){
+        return "required";
+      }
+    }
   },
   optionElDir: {
     type: String,
@@ -38,6 +46,13 @@ Tournois.attachSchema(new SimpleSchema({
           {label:"Match A/R", value:"2M"},
           {label:"Série", value:"PM"}
       ]}
+    },
+    optional: true,
+    custom: function(){
+      // If tournament is Elimination directe
+      if(this.field("typeTournoi").value === "ELD" && this.value === undefined){
+        return "required";
+      }
     }
   },
   optionSerie: {
@@ -51,6 +66,16 @@ Tournois.attachSchema(new SimpleSchema({
           {label:"5", value:"5M"},
           {label:"7", value:"7M"}
       ]}
+    },
+    optional: true,
+    custom: function(){
+      // If tournament has serie of match
+      // if(this.field("optionChmpt" != "CHP")){
+        if(this.field("optionElDir").value === "PM" && this.value === "7M"){
+          // console.log(alert("salut"));
+          return "required";
+        }
+      // }
     }
   },
   joueurs: {
