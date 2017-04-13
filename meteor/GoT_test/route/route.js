93994841d1595358d7624dd6cebd963de83d33bf
Raphbub <raph.bub@gmail.com>
2017-04-13 14:50:13 +0200

    debut ajout compte user

--- meteor/GoT_test/route/route.js
+++ meteor/GoT_test/route/route.js
@@ -7,8 +7,16 @@ Router.route('/', function () {
   this.render('homepage');
 });
 
-Router.route('/creer', function () {
-  this.render('page_creer');
+Router.route('/creer', {
+  template: 'page_creer',
+  onBeforeAction: function() {
+    let currentUser = Meteor.userId();
+    if (currentUser) {
+      this.next();
+    } else {
+      this.render("login");
+    }
+  }
 });
 
 Router.route('/suivre', function () {
@@ -27,6 +35,13 @@ Router.route('/about', function () {
   this.render('page_about');
 });
 
+Router.route('/senregistrer', function () {
+  this.render('page_enregistrer');
+});
+
+Router.route('/login', function () {
+  this.render('login');
+});
 
 Router.route('/:_id', {
     template: 'page_suivre',
