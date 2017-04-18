129d8293bcea7eabcb869fbac469e9c5cecf3c9d
Raphbub <raph.bub@gmail.com>
2017-04-18 13:16:35 +0200

    ajout fonction suppression de tournoi dans gerer

--- meteor/GoT_test/client/templates/gerer.js
+++ meteor/GoT_test/client/templates/gerer.js
@@ -4,3 +4,14 @@ Template.page_gerer.helpers({
     return Tournois.find({admin: currentUser}, {sort: {date: -1}});
   }
 });
+
+Template.page_gerer.events ({
+  'click .suppr-tournoi': function(event){
+      event.preventDefault();
+      var IdTournoi = this._id;
+      var confirmation = window.confirm("Supprimer définitivement ce tournoi ?");
+      if(confirmation){
+          Tournois.remove({ _id: IdTournoi });
+      }
+  }
+});
