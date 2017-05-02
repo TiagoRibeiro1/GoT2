93ee1b2d23de4c37ea91aab46b37535207b2b15c
lucas.martinez787@gmail.com <lucas.martinez787@gmail.com>
2017-05-02 16:52:12 +0200

    working on classment table

--- meteor/GoT_test/client/templates/suivre.js
+++ meteor/GoT_test/client/templates/suivre.js
@@ -1,9 +1,20 @@
-import { Template } from 'meteor/templating';
+Template.page_suivre.helpers({
+  'matchCrees': function(){
+    console.log(Matchs.find({idTournoi: this._id}));
+  }
+});
 
-
-Template.page_suivre.events({
-  'click .joueurs'() {
-    // test
-    alert('ok');
-  },
+Template.test.helpers({
+  'position': function(){
+    let tournoi = Tournois.findOne({idTournoi: this._id});
+    for(i = 0; i < tournoi.joueurs.length; i++){
+      let array = [];
+      // for(i = 0; i < 10; i++){
+        array.push(i)
+        array.push(tournoi.joueurs[i]);
+        array.push("ciao")
+        return array;
+      // }
+    }
+  }
 });
