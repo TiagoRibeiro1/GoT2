534370f315202f93a2204426ab112fb841a2862a
Jean Ceppi <jeanceppi@protonmail.com>
2017-05-03 11:27:17 +0200

    matchJoue OK, matchgagne NOK

--- meteor/GoT_test/client/templates/suivre.js
+++ meteor/GoT_test/client/templates/suivre.js
@@ -16,5 +16,55 @@ Template.test.helpers({
         return array;
       // }
     }
-  }
+  },
+    matchCount: function() {
+    return Matchs.find().count();
+  },
+    matchJoue: function(nj, idT) {
+        
+        return Matchs.find({ $or: [{"j1.name": nj},{"j2.name": nj}], termine:true, idTournoi:idT}).count();
+        //return e;
+    },
+    
+    matchGagne: function(nj, idT) {
+        
+        //si idTournoi = idT
+        //  si termine = true
+        //    si j1.score = j2.score
+        //        exit
+        //    si j1.name = nj
+        //        si j1.score > j2.score
+        //        alors gagne
+        //        exit
+        //    si j2.name = nj
+        //        si j2.score > j1.score
+        //        alors gagne
+        //        exit
+        //return gagne
+        
+        //termine:true && idTournoi:idT && j1.score != j2.score && [ j1.name = nj && j1.score > j2.score ] || [ j2.name = nj && j1.score < j2.score ]
+        
+        
+        return Matchs.find({
+            termine:true,
+            idTournoi:idT,
+            "j1.score": parseInt("j1.score")
+            /*$or:[{
+                $and:[{
+                    "j1.name": nj
+                },{
+                    "j1.score":{ $gt: "j2.score"} 
+                }]
+            },{
+                $and:[{
+                    "j2.name": nj
+                },{
+                    "j2.score": {$gt: "j1.score"}
+                }]
+            }]*/
+        }).count();
+        
+        //return Matchs.find({ $or: [{"j1.name": nj},{"j2.name": nj}], termine:true, idTournoi:idT}).count();
+        //return e;
+    }
 });
