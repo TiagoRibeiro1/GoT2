ceafffcc6321e69e1963195f623de747a5ebec1c
TiagoRibeiro1 <tiago.nunesribeiro@unil.ch>
2017-05-24 10:52:03 +0200

    changements ...

---
+++ navbar.js
@@ -0,0 +1,11 @@
+Template.navBar.events({
+    'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+    }
+});
+    Template.navBar.helpers({
+    	logo: function() {
+    		return Meteor.absoluteUrl("favicon.png");
+    	}
+    });
